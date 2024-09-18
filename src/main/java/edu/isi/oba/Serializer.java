3ed1998657cbba79a88e865857ee577e39983d66
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2024-09-18 12:00:58 -0500

    add Mixin to alphabetically support properties/schemas

--- src/main/java/edu/isi/oba/Serializer.java
+++ src/main/java/edu/isi/oba/Serializer.java
@@ -1,11 +1,20 @@
 package edu.isi.oba;
 
-import io.swagger.v3.core.util.Json;
-import io.swagger.v3.core.util.Yaml;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContext;
+import io.swagger.v3.oas.integration.SwaggerConfiguration;
+import io.swagger.v3.oas.integration.api.OpenApiContext;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.Paths;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.tags.Tag;
 import io.swagger.v3.parser.OpenAPIV3Parser;
@@ -14,6 +23,7 @@ import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
@@ -126,11 +136,31 @@ class Serializer {
 		openAPI.setPaths(paths);
 		openAPI.components(components);
 
+		// Don't use .sortedOutput(true) because we are using SortedSchemaMixin to alphabetically sort
+		// the desired entries.  Sorting _everything_ alphabetically messes up the YAML file by moving
+		// the info section away from the top of the document, for example.
+		SwaggerConfiguration openApiConfiguration =
+				new SwaggerConfiguration().openAPI(openAPI).prettyPrint(true);
+
+		OpenApiContext ctx =
+				new JaxrsOpenApiContext<>().openApiConfiguration(openApiConfiguration).init();
+
+		// ctx.getOutputYamlMapper().configure(MapperFeature.USE_ANNOTATIONS, true);
+		ctx.getOutputYamlMapper().configure(SerializationFeature.FLUSH_AFTER_WRITE_VALUE, true);
+		ctx.getOutputYamlMapper().configure(SerializationFeature.INDENT_OUTPUT, true);
+		ctx.getOutputYamlMapper().configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+		ctx.getOutputYamlMapper().configure(SerializationFeature.WRITE_ENUM_KEYS_USING_INDEX, true);
+		ctx.getOutputYamlMapper().addMixIn(Schema.class, SortedSchemaMixin.class);
+
 		// write the filename
 		final var content =
 				Optional.ofNullable(saveAsJSON).orElse(false)
-						? Json.pretty().writeValueAsString(openAPI)
-						: Yaml.pretty().writeValueAsString(openAPI);
+						? ctx.getOutputJsonMapper()
+								.writer(new DefaultPrettyPrinter())
+								.writeValueAsString(openAPI)
+						: ctx.getOutputYamlMapper()
+								.writer(new DefaultPrettyPrinter())
+								.writeValueAsString(openAPI);
 		this.openapi_path = dir + File.separator + openapi_file;
 		File file = new File(openapi_path);
 		BufferedWriter writer = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
@@ -166,5 +196,125 @@ class Serializer {
 		securityScheme.setScheme("bearer");
 		securityScheme.setExtensions(extensions);
 		return securityScheme;
+	}
+
+	/** The interface Sorted schema mixin. */
+	@JsonPropertyOrder(
+			value = {
+				"description",
+				"required",
+				"minItems",
+				"maxItems",
+				"type",
+				"format",
+				"nullable",
+				"default",
+				"enum",
+				"readOnly",
+				"writeOnly"
+			},
+			alphabetic = true)
+	abstract static class SortedSchemaMixin {
+
+		@JsonAnyGetter
+		@JsonPropertyOrder(alphabetic = true)
+		public abstract Map<String, Object> getExtensions();
+
+		@JsonAnySetter
+		public abstract void addExtension(String name, Object value);
+
+		@JsonIgnore
+		public abstract boolean getExampleSetFlag();
+
+		@JsonInclude(value = JsonInclude.Include.NON_NULL, content = JsonInclude.Include.ALWAYS)
+		public abstract Object getExample();
+
+		@JsonIgnore
+		public abstract Map<String, Object> getJsonSchema();
+
+		@JsonIgnore
+		public abstract BigDecimal getExclusiveMinimumValue();
+
+		@JsonIgnore
+		public abstract BigDecimal getExclusiveMaximumValue();
+
+		@JsonIgnore
+		public abstract Map<String, Schema> getPatternProperties();
+
+		@JsonIgnore
+		public abstract Schema getContains();
+
+		@JsonIgnore
+		public abstract String get$id();
+
+		// TODO: Figure out if anchors for enums can be supported.
+		@JsonIgnore
+		public abstract String get$anchor();
+
+		@JsonIgnore
+		public abstract String get$schema();
+
+		@JsonIgnore
+		public abstract Set<String> getTypes();
+
+		@JsonIgnore
+		public abstract Object getJsonSchemaImpl();
+
+		@JsonIgnore
+		public abstract List<Schema> getPrefixItems();
+
+		@JsonIgnore
+		public abstract String getContentEncoding();
+
+		@JsonIgnore
+		public abstract String getContentMediaType();
+
+		@JsonIgnore
+		public abstract Schema getContentSchema();
+
+		@JsonIgnore
+		public abstract Schema getPropertyNames();
+
+		@JsonIgnore
+		public abstract Object getUnevaluatedProperties();
+
+		@JsonIgnore
+		public abstract Integer getMaxContains();
+
+		@JsonIgnore
+		public abstract Integer getMinContains();
+
+		@JsonIgnore
+		public abstract Schema getAdditionalItems();
+
+		@JsonIgnore
+		public abstract Schema getUnevaluatedItems();
+
+		@JsonIgnore
+		public abstract Schema getIf();
+
+		@JsonIgnore
+		public abstract Schema getElse();
+
+		@JsonIgnore
+		public abstract Schema getThen();
+
+		@JsonIgnore
+		public abstract Map<String, Schema> getDependentSchemas();
+
+		@JsonIgnore
+		public abstract Map<String, List<String>> getDependentRequired();
+
+		@JsonIgnore
+		public abstract String get$comment();
+
+		@JsonIgnore
+		public abstract List<Object> getExamples();
+
+		@JsonIgnore
+		public abstract Object getConst();
+
+		@JsonIgnore
+		public abstract Boolean getBooleanSchemaValue();
 	}
 }
