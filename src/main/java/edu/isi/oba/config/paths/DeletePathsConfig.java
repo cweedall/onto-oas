59cf397dc56094fcde543d4cc1a7dbd2b6eb1fdc
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2025-05-23 14:54:32 -0500

    use jackson annotations; use global config names; fix naming

--- src/main/java/edu/isi/oba/config/paths/DeletePathsConfig.java
+++ src/main/java/edu/isi/oba/config/paths/DeletePathsConfig.java
@@ -1,97 +1,58 @@
 package edu.isi.oba.config.paths;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import edu.isi.oba.config.flags.ConfigFlagType;
-import edu.isi.oba.config.flags.ConfigFlags;
-import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import edu.isi.oba.config.ConfigPropertyNames;
+import edu.isi.oba.config.flags.GlobalFlags;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public final class DeletePathsConfig extends ConfigFlags {
-	private Delete_By_Key delete_by_key;
-
-	/**
-	 * Get the {@link Delete_By_Key} config (a sub-property within {@link DeletePathsConfig}).
-	 *
-	 * @return a {@link DeletePathsConfig}
-	 */
-	public Delete_By_Key getDelete_by_key() {
-		return delete_by_key;
-	}
-
-	/**
-	 * Set the {@link Delete_By_Key}, if it exists in the config file. This is the configuration for
-	 * DELETE paths.
-	 *
-	 * @param {delete_by_key} A {@link Delete_By_Key}
-	 */
-	public void setDelete_by_key(Delete_By_Key delete_by_key) {
-		if (delete_by_key == null) {
-			this.delete_by_key = new Delete_By_Key();
-		} else {
-			this.delete_by_key = delete_by_key;
-		}
-
-		this.configFlags.putAll(this.delete_by_key.getConfigFlags());
-	}
+@JsonRootName(ConfigPropertyNames.DELETE_PATHS)
+public final class DeletePathsConfig {
+	@JsonProperty(ConfigPropertyNames.DELETE_BY_KEY)
+	public final DeleteByKey deleteByKey = new DeleteByKey();
 
 	@JsonIgnoreProperties(ignoreUnknown = true)
-	public static class Delete_By_Key extends ConfigFlags {
+	@JsonRootName(ConfigPropertyNames.DELETE_BY_KEY)
+	public static class DeleteByKey {
 		// Default key name to be "id";
-		private String key_name = "id";
-		private String key_name_in_text = this.key_name;
+		@JsonProperty(ConfigPropertyNames.KEY_NAME)
+		private String keyName = "id";
+
+		@JsonProperty(ConfigPropertyNames.KEY_NAME_IN_TEXT)
+		private String keyNameInText = this.keyName;
 
 		// Default key type to be STRING.  Used within the application for enum convenience.
-		private PathKeyType key_type = PathKeyType.STRING;
-		// Default key type to be "string".  Used as a convenience for setting the values from the
-		// configuration file.
-		private String key_datatype = this.key_type.toString();
+		private PathKeyType keyType = PathKeyType.STRING;
 
-		Delete_By_Key() {
-			this.configFlags.putAll(Map.ofEntries(Map.entry(ConfigFlagType.PATH_DELETE_BY_ID, false)));
+		DeleteByKey() {
+			GlobalFlags.setFlag(ConfigPropertyNames.DELETE_BY_KEY_ENABLE, false);
 		}
 
-		public Boolean getEnable() {
-			return this.configFlags.get(ConfigFlagType.PATH_DELETE_BY_ID);
-		}
-
-		public void setEnable(Boolean enable) {
+		@JsonSetter(ConfigPropertyNames.ENABLE)
+		private void setEnable(Boolean enable) {
 			if (enable != null) {
-				this.configFlags.put(ConfigFlagType.PATH_DELETE_BY_ID, enable);
+				GlobalFlags.setFlag(ConfigPropertyNames.DELETE_BY_KEY_ENABLE, enable);
 			}
 		}
 
-		public String getKey_name() {
-			return this.key_name;
+		public String getKeyName() {
+			return this.keyName;
 		}
 
-		public void setKey_name(String key_name) {
-			if (key_name != null && !key_name.isBlank()) {
-				this.key_name = key_name;
-			}
+		public String getKeyNameInText() {
+			return this.keyNameInText;
 		}
 
-		public String getKey_name_in_text() {
-			return this.key_name_in_text;
+		public PathKeyType getKeyType() {
+			return this.keyType;
 		}
 
-		public void setKey_name_in_text(String key_name_in_text) {
-			if (key_name_in_text != null && !key_name_in_text.isBlank()) {
-				this.key_name_in_text = key_name_in_text;
-			}
-		}
-
-		public PathKeyType getKey_type() {
-			return this.key_type;
-		}
-
-		public String getKey_datatype() {
-			return this.key_datatype;
-		}
-
-		public void setKey_datatype(String key_datatype) {
-			if (key_datatype != null && !key_datatype.isBlank()) {
-				this.key_type = PathKeyType.valueOfLabel(key_datatype);
-				this.key_datatype = key_datatype;
+		@JsonSetter(ConfigPropertyNames.KEY_TYPE)
+		private void setKeyDatatype(String keyType) {
+			if (keyType != null && !keyType.isBlank()) {
+				this.keyType = PathKeyType.valueOfLabel(keyType);
 			}
 		}
 	}
--- src/main/java/edu/isi/oba/config/paths/PathConfig.java
+++ src/main/java/edu/isi/oba/config/paths/PathConfig.java
@@ -1,63 +1,57 @@
 package edu.isi.oba.config.paths;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import edu.isi.oba.config.flags.ConfigFlagType;
-import edu.isi.oba.config.flags.ConfigFlags;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import edu.isi.oba.config.ConfigPropertyNames;
+import edu.isi.oba.config.flags.GlobalFlags;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.semanticweb.owlapi.model.IRI;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PathConfig extends ConfigFlags {
+@JsonRootName(ConfigPropertyNames.PATH_CONFIG)
+public class PathConfig {
+	@JsonProperty(ConfigPropertyNames.PATHS_FOR_CLASSES)
 	private Set<PathsForClassConfig> pathsForClasses = new HashSet<>();
-	private DeletePathsConfig delete_paths;
-	private GetPathsConfig get_paths;
-	private PostPathsConfig post_paths;
-	private PutPathsConfig put_paths;
-	private SearchPathsConfig search_paths;
+
+	@JsonProperty(ConfigPropertyNames.DELETE_PATHS)
+	private DeletePathsConfig deletePaths;
+
+	@JsonProperty(ConfigPropertyNames.GET_PATHS)
+	private GetPathsConfig getPaths;
+
+	@JsonProperty(ConfigPropertyNames.POST_PATHS)
+	private PostPathsConfig postPaths;
+
+	@JsonProperty(ConfigPropertyNames.PUT_PATHS)
+	private PutPathsConfig putPaths;
+
+	@JsonProperty(ConfigPropertyNames.SEARCH_PATHS)
+	private SearchPathsConfig searchPaths;
 
 	public PathConfig() {
-		this.configFlags.putAll(
-				Map.ofEntries(
-						Map.entry(ConfigFlagType.DISABLE_ALL_PATHS, false),
-						Map.entry(ConfigFlagType.PATH_PATCH, false),
-						Map.entry(ConfigFlagType.USE_COMMON_DEFAULT_PATH_RESPONSES, true),
-						Map.entry(ConfigFlagType.USE_KEBAB_CASE_PATHS, false)));
+		GlobalFlags.setFlag(ConfigPropertyNames.DISABLE_ALL_PATHS, false);
+		GlobalFlags.setFlag(ConfigPropertyNames.USE_COMMON_DEFAULT_PATH_RESPONSES, true);
+		GlobalFlags.setFlag(ConfigPropertyNames.USE_KEBAB_CASE_PATHS, false);
 	}
 
-	public Boolean getDisable_all_paths() {
-		return this.configFlags.get(ConfigFlagType.DISABLE_ALL_PATHS);
+	@JsonSetter(ConfigPropertyNames.DISABLE_ALL_PATHS)
+	private void setDisableAllPaths(Boolean disableAllPaths) {
+		GlobalFlags.setFlag(ConfigPropertyNames.DISABLE_ALL_PATHS, disableAllPaths);
 	}
 
-	public void setDisable_all_paths(Boolean disable_all_paths) {
-		this.configFlags.put(ConfigFlagType.DISABLE_ALL_PATHS, disable_all_paths);
+	@JsonSetter(ConfigPropertyNames.USE_COMMON_DEFAULT_PATH_RESPONSES)
+	private void setUseCommonDefaultPathResponses(Boolean useCommonDefaultPathResponses) {
+		GlobalFlags.setFlag(
+				ConfigPropertyNames.USE_COMMON_DEFAULT_PATH_RESPONSES, useCommonDefaultPathResponses);
 	}
 
-	public Boolean getEnable_patch_paths() {
-		return this.configFlags.get(ConfigFlagType.PATH_PATCH);
-	}
-
-	public void setEnable_patch_paths(Boolean enable_patch_paths) {
-		this.configFlags.put(ConfigFlagType.PATH_PATCH, enable_patch_paths);
-	}
-
-	public Boolean getUse_common_default_path_responses() {
-		return this.configFlags.get(ConfigFlagType.USE_COMMON_DEFAULT_PATH_RESPONSES);
-	}
-
-	public void setUse_common_default_path_responses(Boolean use_common_default_path_responses) {
-		this.configFlags.put(
-				ConfigFlagType.USE_COMMON_DEFAULT_PATH_RESPONSES, use_common_default_path_responses);
-	}
-
-	public Boolean getUse_kebab_case_paths() {
-		return this.configFlags.get(ConfigFlagType.USE_KEBAB_CASE_PATHS);
-	}
-
-	public void setUse_kebab_case_paths(Boolean use_kebab_case_paths) {
-		this.configFlags.put(ConfigFlagType.USE_KEBAB_CASE_PATHS, use_kebab_case_paths);
+	@JsonSetter(ConfigPropertyNames.USE_KEBAB_CASE_PATHS)
+	private void setUseKebabCasePaths(Boolean useKebabCasePaths) {
+		GlobalFlags.setFlag(ConfigPropertyNames.USE_KEBAB_CASE_PATHS, useKebabCasePaths);
 	}
 
 	/**
@@ -82,37 +76,12 @@ public class PathConfig extends ConfigFlags {
 	}
 
 	/**
-	 * Set all {@link PathsForClassConfig}, if any exist in the config file.
-	 *
-	 * @param {pathsForClasses} a {@link Set} of {@link PathsForClassConfig}
-	 */
-	public void setPaths_for_classes(Set<PathsForClassConfig> pathsForClasses) {
-		this.pathsForClasses = pathsForClasses;
-	}
-
-	/**
 	 * Get the {@link DeletePathsConfig} may be null (because it doesn't exist in the config file).
 	 *
 	 * @return a {@link DeletePathsConfig}
 	 */
-	public DeletePathsConfig getDelete_paths() {
-		return this.delete_paths;
-	}
-
-	/**
-	 * Set the DeletePathsConfig, if it exists in the config file. This is the configuration for
-	 * DELETE paths.
-	 *
-	 * @param {delete_paths} A {@link DeletePathsConfig}
-	 */
-	public void setDelete_paths(DeletePathsConfig delete_paths) {
-		if (delete_paths == null) {
-			this.delete_paths = new DeletePathsConfig();
-		} else {
-			this.delete_paths = delete_paths;
-		}
-
-		this.configFlags.putAll(this.delete_paths.getConfigFlags());
+	public DeletePathsConfig getDeletePaths() {
+		return this.deletePaths;
 	}
 
 	/**
@@ -120,24 +89,8 @@ public class PathConfig extends ConfigFlags {
 	 *
 	 * @return a {@link GetPathsConfig}
 	 */
-	public GetPathsConfig getGet_paths() {
-		return this.get_paths;
-	}
-
-	/**
-	 * Set the GetPathsConfig, if it exists in the config file. This is the configuration for GET
-	 * paths.
-	 *
-	 * @param {get_paths} A {@link GetPathsConfig}
-	 */
-	public void setGet_paths(GetPathsConfig get_paths) {
-		if (get_paths == null) {
-			this.get_paths = new GetPathsConfig();
-		} else {
-			this.get_paths = get_paths;
-		}
-
-		this.configFlags.putAll(this.get_paths.getConfigFlags());
+	public GetPathsConfig getGetPaths() {
+		return this.getPaths;
 	}
 
 	/**
@@ -145,24 +98,8 @@ public class PathConfig extends ConfigFlags {
 	 *
 	 * @return a {@link PostPathsConfig}
 	 */
-	public PostPathsConfig getPost_paths() {
-		return this.post_paths;
-	}
-
-	/**
-	 * Set the PostPathsConfig, if it exists in the config file. This is the configuration for POST
-	 * paths.
-	 *
-	 * @param {post_paths} A {@link PostPathsConfig}
-	 */
-	public void setPost_paths(PostPathsConfig post_paths) {
-		if (post_paths == null) {
-			this.post_paths = new PostPathsConfig();
-		} else {
-			this.post_paths = post_paths;
-		}
-
-		this.configFlags.putAll(this.post_paths.getConfigFlags());
+	public PostPathsConfig getPostPaths() {
+		return this.postPaths;
 	}
 
 	/**
@@ -170,24 +107,8 @@ public class PathConfig extends ConfigFlags {
 	 *
 	 * @return a {@link PutPathsConfig}
 	 */
-	public PutPathsConfig getPut_paths() {
-		return this.put_paths;
-	}
-
-	/**
-	 * Set the PutPathsConfig, if it exists in the config file. This is the configuration for PUT
-	 * paths.
-	 *
-	 * @param {put_paths} A {@link PutPathsConfig}
-	 */
-	public void setPut_paths(PutPathsConfig put_paths) {
-		if (put_paths == null) {
-			this.put_paths = new PutPathsConfig();
-		} else {
-			this.put_paths = put_paths;
-		}
-
-		this.configFlags.putAll(this.put_paths.getConfigFlags());
+	public PutPathsConfig getPutPaths() {
+		return this.putPaths;
 	}
 
 	/**
@@ -195,23 +116,7 @@ public class PathConfig extends ConfigFlags {
 	 *
 	 * @return a {@link SearchPathsConfig}
 	 */
-	public SearchPathsConfig getSearch_paths() {
-		return this.search_paths;
-	}
-
-	/**
-	 * Set the SearchPathsConfig, if it exists in the config file. This is the configuration for PUT
-	 * paths.
-	 *
-	 * @param {search_paths} A {@link SearchPathsConfig}
-	 */
-	public void setSearch_paths(SearchPathsConfig search_paths) {
-		if (search_paths == null) {
-			this.search_paths = new SearchPathsConfig();
-		} else {
-			this.search_paths = search_paths;
-		}
-
-		this.configFlags.putAll(this.search_paths.getConfigFlags());
+	public SearchPathsConfig getSearchPaths() {
+		return this.searchPaths;
 	}
 }
