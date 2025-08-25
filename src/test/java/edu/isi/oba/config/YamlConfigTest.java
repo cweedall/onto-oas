b423011c0e067cf6395d1dca7c55ca02a0852ef3
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2025-08-25 16:24:15 -0500

    refactor: move applicable unit tests to YamlConfigTest

--- src/test/java/edu/isi/oba/config/YamlConfigTest.java
+++ src/test/java/edu/isi/oba/config/YamlConfigTest.java
@@ -2,11 +2,14 @@ package edu.isi.oba.config;
 
 import static edu.isi.oba.utils.ObaUtils.get_yaml_data;
 
+import edu.isi.oba.BaseTest;
 import java.util.Set;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class YamlConfigTest {
+@Disabled
+public class YamlConfigTest extends BaseTest {
 	@Test
 	public void getSelectedClasses() {
 		String config_test_file_path = "examples/dbpedia/config_music.yaml";
@@ -15,5 +18,51 @@ public class YamlConfigTest {
 				Set.of("http://dbpedia.org/ontology/Genre", "http://dbpedia.org/ontology/Band");
 		Set<String> config = config_data.getClasses();
 		Assertions.assertEquals(expected, config);
+	}
+
+	/**
+	 * This test attempts to load a local ontology.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testLocalFile() throws Exception {
+		final var local_ontology = "src/test/config/mcat_reduced.yaml";
+		final var config_data = get_yaml_data(local_ontology);
+		Assertions.assertEquals(false, config_data.getOntologies().isEmpty());
+	}
+
+	/**
+	 * This test attempts to load a config in a folder with spaces.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testSpacesInPath() throws Exception {
+		final var local_ontology = "examples/example with spaces/config.yaml";
+		final var config_data = get_yaml_data(local_ontology);
+		Assertions.assertEquals(false, config_data.getOntologies().isEmpty());
+	}
+
+	/**
+	 * This test attempts to run OBA with an online ontology through a URI. The ontology is hosted in
+	 * GitHub, but there is a small risk of the test not passing due to the unavailability of the
+	 * ontology.
+	 *
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testRemoteOntology() throws Exception {
+		final var example_remote = "src/test/config/pplan.yaml";
+		final var config_data = get_yaml_data(example_remote);
+		Assertions.assertEquals(false, config_data.getOntologies().isEmpty());
+	}
+
+	/** Test an ontology (very simple, two classes) with a missing import */
+	@Test
+	public void testMissingImportOntology() throws Exception {
+		final var example_remote = "src/test/resources/missing_import/config.yaml";
+		final var config_data = get_yaml_data(example_remote);
+		Assertions.assertEquals(false, config_data.getOntologies().isEmpty());
 	}
 }
