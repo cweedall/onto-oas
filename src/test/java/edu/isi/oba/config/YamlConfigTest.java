a1104236a37b37c922340954965d63e374a983e7
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2025-09-01 20:31:44 -0500

    test: update ObaUtils references

--- src/test/java/edu/isi/oba/config/YamlConfigTest.java
+++ src/test/java/edu/isi/oba/config/YamlConfigTest.java
@@ -1,11 +1,9 @@
 package edu.isi.oba.config;
 
-import static edu.isi.oba.utils.ObaUtils.get_yaml_data;
-
 import edu.isi.oba.BaseTest;
 import edu.isi.oba.config.flags.GlobalFlags;
 import edu.isi.oba.exceptions.ConfigValidationException;
-import edu.isi.oba.utils.ObaUtils;
+import edu.isi.oba.utils.yaml.YamlUtils;
 import java.io.File;
 import java.util.Set;
 import org.junit.jupiter.api.Assertions;
@@ -18,7 +16,7 @@ public class YamlConfigTest extends BaseTest {
 	@Test
 	public void getSelectedClasses() {
 		String config_test_file_path = "examples/dbpedia/config_music.yaml";
-		YamlConfig config_data = get_yaml_data(config_test_file_path);
+		YamlConfig config_data = YamlUtils.getYamlData(config_test_file_path);
 		Set<String> expected =
 				Set.of("http://dbpedia.org/ontology/Genre", "http://dbpedia.org/ontology/Band");
 		Set<String> config = config_data.getExtraClassSchemas();
@@ -28,28 +26,28 @@ public class YamlConfigTest extends BaseTest {
 	@Test
 	public void testLocalFile() throws Exception {
 		final var local_ontology = "src/test/config/mcat_reduced.yaml";
-		final var config_data = get_yaml_data(local_ontology);
+		final var config_data = YamlUtils.getYamlData(local_ontology);
 		Assertions.assertFalse(config_data.getOntologies().isEmpty());
 	}
 
 	@Test
 	public void testSpacesInPath() throws Exception {
 		final var local_ontology = "examples/example with spaces/config.yaml";
-		final var config_data = get_yaml_data(local_ontology);
+		final var config_data = YamlUtils.getYamlData(local_ontology);
 		Assertions.assertFalse(config_data.getOntologies().isEmpty());
 	}
 
 	@Test
 	public void testRemoteOntology() throws Exception {
 		final var example_remote = "src/test/config/pplan.yaml";
-		final var config_data = get_yaml_data(example_remote);
+		final var config_data = YamlUtils.getYamlData(example_remote);
 		Assertions.assertFalse(config_data.getOntologies().isEmpty());
 	}
 
 	@Test
 	public void testMissingImportOntology() throws Exception {
 		final var example_remote = "src/test/resources/missing_import/config.yaml";
-		final var config_data = get_yaml_data(example_remote);
+		final var config_data = YamlUtils.getYamlData(example_remote);
 		Assertions.assertFalse(config_data.getOntologies().isEmpty());
 	}
 
@@ -57,7 +55,7 @@ public class YamlConfigTest extends BaseTest {
 	public void missingFile() throws OWLOntologyCreationException {
 		final var missing_file = "src/test/config/missing_file.yaml";
 		try {
-			final var config_data = ObaUtils.get_yaml_data(missing_file);
+			final var config_data = YamlUtils.getYamlData(missing_file);
 			Assertions.fail("Missing file: If no exception is launched, fail test");
 		} catch (Exception e) {
 			// pass test if there is an exception
