ff3c0eeaff0e7aca460590ad7877e141e2699cab
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2025-09-22 12:42:16 -0500

    refactor: new reason util for common factory/creation

---
+++ src/main/java/edu/isi/oba/ontology/reasoner/ReasonerUtil.java
@@ -0,0 +1,14 @@
+package edu.isi.oba.ontology.reasoner;
+
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
+import org.semanticweb.owlapi.reasoner.OWLReasonerFactory;
+import org.semanticweb.owlapi.reasoner.structural.StructuralReasonerFactory;
+
+public class ReasonerUtil {
+	private static final OWLReasonerFactory factory = new StructuralReasonerFactory();
+
+	public static OWLReasoner createReasoner(OWLOntology ontology) {
+		return factory.createReasoner(ontology);
+	}
+}
