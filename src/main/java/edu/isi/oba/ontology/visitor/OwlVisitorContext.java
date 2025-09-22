9ea13b2978c029c23ddc7c6d7fb719fb3798251b
Christopher Weedall <5010253+cweedall@users.noreply.github.com>
2025-09-22 12:57:06 -0500

    refactor:  new constructor, ordering, missing methods

--- src/main/java/edu/isi/oba/ontology/visitor/OwlVisitorContext.java
+++ src/main/java/edu/isi/oba/ontology/visitor/OwlVisitorContext.java
@@ -1,10 +1,12 @@
 package edu.isi.oba.ontology.visitor;
 
+import edu.isi.oba.config.YamlConfig;
 import io.swagger.v3.oas.models.media.Schema;
 import java.util.Map;
 import java.util.Set;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 /** Public read-only view of {@link VisitorContext} for use in helper classes. */
 public interface OwlVisitorContext {
@@ -27,7 +29,25 @@ public interface OwlVisitorContext {
 
 	Set<OWLClass> getProcessedClasses();
 
+	Set<OWLClass> getProcessedRestrictionClasses();
+
+	boolean isRestrictionClassProcessed(OWLClass cls);
+
 	Map<String, Map<String, String>> getMarkdownGenerationMap();
+
+	Set<String> getPropertyNames();
+
+	Set<String> getEnumProperties();
+
+	Set<String> getFunctionalProperties();
+
+	String getCurrentlyProcessedPropertyName();
+
+	YamlConfig getConfigData();
+
+	OWLReasoner getReasoner();
+
+	OWLClass getOwlThing();
 
 	// Add more getters as needed
 }
